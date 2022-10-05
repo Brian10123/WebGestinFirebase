45e937f0664f38c39c23b468facd1b9204ddb472
Rafael Rodriguez Viau <rodriguezviaurafael@gmail.com>
2022-10-04 23:39:20 -0300

    Added navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { DashboardComponent } from './Components/dashboard/dashboard.component';
+import { NavigationBarComponent } from './Components/navigation-bar/navigation-bar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    DashboardComponent
+    DashboardComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
